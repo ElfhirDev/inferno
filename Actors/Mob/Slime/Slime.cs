3c31611d20d546c5be8fee55e58b4dccfbcf16c3
Elfhir <ta.thanhliem@gmail.com>
2020-07-01 17:36:06 +0200

    New sounds FX

--- Actors/Mob/Slime/Slime.cs
+++ Actors/Mob/Slime/Slime.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d9484718ffbd5af8dd22a615c95268322bb975432b9c90b3c748e6644b53647
-size 7652
+oid sha256:f50bd64f6aafe3dd6c46665e3a3178cccd632e4f7b0a3d146d28cd96dae3e02a
+size 7768
