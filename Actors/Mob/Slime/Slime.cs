8719f5096a14e4c7edbdde9558ae202575c6c637
Elfhir <ta.thanhliem@gmail.com>
2020-07-03 16:49:15 +0200

    Update Slime IA : trying to follow the player and attacking him when at range.

--- Actors/Mob/Slime/Slime.cs
+++ Actors/Mob/Slime/Slime.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50bd64f6aafe3dd6c46665e3a3178cccd632e4f7b0a3d146d28cd96dae3e02a
-size 7768
+oid sha256:ebc512c4dbf60a9736171684d6f140c1d46d7ace39e4483b683e1c458ed1879a
+size 9707
